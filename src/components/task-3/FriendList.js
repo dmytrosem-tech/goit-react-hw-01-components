e7e60e821fa643dca31e426a232afcc23c6b2437
Dmytro Semeruk <deemoon83@gmail.com>
2021-10-01 12:30:55 +0300

    Без стилей

--- src/components/task-3/FriendList.js
+++ src/components/task-3/FriendList.js
@@ -1,38 +1,38 @@
 import React from "react";
-import FriendsListItem from "./FriendsListItem";
+import PropTypes from "prop-types";
 
 export default function FriendList({ friends }) {
   return (
     <ul className="friends">
-      {friends.map((friend) => (
-        <li className="friends__item" key={friend.id}>
+      {friends.map(({ id, isOnline, avatar, name }) => (
+        <li className="friends__item" key={id}>
           <span
             className={
-              friend.isOnline
+              isOnline
                 ? "friends__status friends__status--online"
                 : "friends__status friends__status--offline"
             }
           ></span>
           <img
             className="friends__avatar"
-            src={friend.avatar}
+            src={avatar}
             alt="avatar"
             width="48"
           />
-          <p className="friends__name">{friend.name}</p>
+          <p className="friends__name">{name}</p>
         </li>
       ))}
     </ul>
   );
 }
 
-// FriendList.propTypes = {
-//   friends: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.number.isRequired,
-//       avatar: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       isOnline: PropTypes.bool.isRequired,
-//     }),
-//   ),
-// };
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
--- src/components/task-4/Transaction-History.js
+++ src/components/task-4/Transaction-History.js
@@ -1,13 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function TransactionHistory(items) {
-  return <div>lol</div>;
+export default function TransactionHistory({ items }) {
+  return (
+    <table className="transactions">
+      <thead className="ransactions__head">
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody className="transactions__table">
+        {items.map(({ id, type, amount, currency }) => (
+          <tr className="transaction__item" key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-// TransactionHistory.propTypes = {
-//   key: PropTypes.number.isRequired,
-//   friendName: PropTypes.string.isRequired,
-//   status: PropTypes.bool.isRequired,
-//   friendAvatar: PropTypes.string.isRequired,
-// };
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ),
+};
