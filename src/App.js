1044744fe851c32ee9014470f445771bb1edd507
Dmytro Semeruk <deemoon83@gmail.com>
2021-10-05 10:38:45 +0300

    fin-2

--- src/App.js
+++ src/App.js
@@ -10,42 +10,21 @@ import friendsData from "./components/FriendsList/friends.json";
 import TransactionHistory from "./components/Transactions/Transactions";
 import transactionsData from "./components/Transactions/transaction-data.json";
 
-// ---------------Profile--------------------->
-const profileMarkup = (
-  <Profile
-    userName={user.name}
-    tag={user.tag}
-    avatarUrl={user.avatar}
-    location={user.location}
-    followers={user.stats.followers}
-    views={user.stats.views}
-    likes={user.stats.likes}
-  />
-);
-
-// -------------Stats-------------------------->
-const statsMarkup = (
-  <Section title="Upload stats" stats={statisticalData}></Section>
-);
-
-// ------------FriendList---------------------->
-const friendsListMarkup = <FriendsList friends={friendsData} />;
-
-// ------------Transactions-------------------->
-const transactionsMarkup = <TransactionHistory items={transactionsData} />;
-
-// ------------joke---------------------------->
-const jopka = <div>Ж..А НАСТАЛА</div>;
-
-// ---------------------------------------------------------RENDER---------------------------------------------------------->
 export default function App() {
   return (
     <div>
-      {profileMarkup}
-      {statsMarkup}
-      {friendsListMarkup}
-      {transactionsMarkup}
-      {jopka}
+      <Profile
+        userName={user.name}
+        tag={user.tag}
+        avatarUrl={user.avatar}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Section title="Upload stats" stats={statisticalData}></Section>
+      <FriendsList friends={friendsData} />
+      <TransactionHistory items={transactionsData} />
     </div>
   );
 }
